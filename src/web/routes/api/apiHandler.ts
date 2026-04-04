f649d9c7db0da931b6344b4392bdf5be7d83cc1a
aidan-walden <aidan-walden@users.noreply.github.com>
2026-04-04 12:54:04 -0400

    refactor: separate hono routes more cleanly

--- src/web/routes/api/apiHandler.ts
+++ src/web/routes/api/apiHandler.ts
@@ -1,29 +1,9 @@
 import { Hono } from "hono";
-import type Bot from "../../../models/Bot";
-import type { Snowflake } from "discord.js";
+import type { AppContext } from "../../context";
+import guilds from "./guilds/guilds";
 
-const api = new Hono();
+const api = new Hono<AppContext>();
 
-export interface GuildListingItem {
-  name: string;
-  id: Snowflake;
-  icon: string | null;
-  available: boolean;
-}
+api.route("/guilds", guilds);
 
-export function createApiHandler(bot: Bot) {
-  // TODO: Dynamically create routes like we do with commands
-  api.get("/listGuilds", (c) => {
-    const guilds = bot.guilds.cache.map((guild) => {
-      return {
-        name: guild.name,
-        id: guild.id,
-        icon: guild.icon,
-        available: guild.available,
-      } as GuildListingItem;
-    });
-    return c.json(guilds);
-  });
-
-  return api;
-}
+export default api;
