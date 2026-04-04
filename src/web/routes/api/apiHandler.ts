0e39c2ada37d8c8ba5e4bb5d23f0a12107e62644
aidan-walden <aidan-walden@users.noreply.github.com>
2026-04-04 13:15:48 -0400

    feat: wip process footprint route

--- src/web/routes/api/apiHandler.ts
+++ src/web/routes/api/apiHandler.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import type { AppContext } from "../../context";
 import guilds from "./guilds/guilds";
+import metrics from "./metrics/metrics";
 
 const api = new Hono<AppContext>();
 
 api.route("/guilds", guilds);
+api.route("/metrics", metrics);
 
 export default api;
