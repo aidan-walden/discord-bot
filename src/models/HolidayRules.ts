86cb4643433cf9fac6f9474c13d1321e0c4310a9
aidan-walden <aidan-walden@users.noreply.github.com>
2026-04-06 16:20:51 -0400

    feat: finish defining holiday rules

--- src/models/HolidayRules.ts
+++ src/models/HolidayRules.ts
@@ -9,14 +9,56 @@ interface HolidayRule {
 	} | null;
 }
 
+// Defines all holidays to watch the calendar for, and what date ranges should be considered "active" for each
 // Smaller index in this list = higher priority, in the case that 2 holidays overlap
 // If getRange returns null, then that holiday is inactive for the supplied year
 const HOLIDAY_RULES: HolidayRule[] = [
+	{
+		holiday: Holiday.Xmas,
+		getRange: (year: number) => ({
+			start: new Date(year, 11),
+			end: getEod(year, 11, 31),
+		}),
+	},
 	{
 		holiday: Holiday.Thanksgiving,
 		getRange: (year: number) => ({
 			start: getNthWeekdayOfMonth(year, 10, 4, 4), // 4th Thursday of November
 			end: getEod(year, 10, 30),
+		}),
+	},
+	{
+		holiday: Holiday.USAElection,
+		getRange: (year: number) => {
+			if (year % 4 !== 0) {
+				return null;
+			}
+
+			return {
+				start: new Date(year, 10),
+				end: getEod(year, 10, 30),
+			};
+		},
+	},
+	{
+		holiday: Holiday.Halloween,
+		getRange: (year: number) => ({
+			start: new Date(year, 9),
+			end: getEod(year, 9, 31),
+		}),
+	},
+	{
+		holiday: Holiday.IndependenceDay,
+		getRange: (year: number) => ({
+			start: new Date(year, 6),
+			end: getEod(year, 6, 31),
+		}),
+	},
+	{
+		holiday: Holiday.AprilFools,
+		getRange: (year: number) => ({
+			start: new Date(year, 3, 1),
+			end: getEod(year, 3, 1),
 		}),
 	},
 ];
